b7db5f54896bc5661599f84fd91095943d8c2d30
kucheli mutambi <kuchelideogratius@gmail.com>
2023-05-09 20:45:11 +0300

    Update 13-insert_number.c

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,70 +1,28 @@
 #include "lists.h"
-
 /**
- * insert_node - malloc and insert node into sorted singly linked list
- * @head: pointer to head of linked list
- * @number: data for new node
- * Return: address of new node, or NULL if failed
+ * *insert_node - function in C that inserts a number into a sorted singly linked list.
+ * @head: is
+ * @number: is
+ * Return: the address of the new node, or NULL if it failed
  */
-
 listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *tmp = NULL;
-	listint_t *new = NULL;
-
+	listint_t *new , *aux1, *aux2;
 	if (!head)
 		return (NULL);
-
-	/* malloc new node */
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
 	new->n = number;
-	new->next = NULL;
-
-	/* if no linked list, insert node as the only member */
-	if (*head == NULL)
+	aux1 = *head;
+	if (!aux1 || aux1->n >= number)
 	{
-		*head = new;
-		(*head)->next = NULL;
+		new->next = aux1, *head = new;
 		return (new);
 	}
-	/* if only one node in linked list, do comparision and insert */
-	if ((*head)->next == NULL)
-	{
-		if ((*head)->n < new->n)
-			(*head)->next = new;
-		else
-		{
-			new->next = *head;
-			*head = new;
-		}
-		return (new);
-	}
-
-	/* if lots of nodes in linked list, do comparision and insert */
-	tmp = *head;
-	while (tmp->next != NULL)
-	{
-		/* if new node num is smaller than first node, insert */
-		if (new->n < tmp->n)
-		{
-			new->next = tmp;
-			*head = new;
-			return (new);
-		}
-		/* if new node num is the same as an existing node, insert */
-		/* compare previous node and next node, insert in between */
-		if (((new->n > tmp->n) && (new->n < (tmp->next)->n)) ||
-		    (new->n == tmp->n))
-		{
-			new->next = tmp->next;
-			tmp->next = new;
-			return (new);
-		}
-		tmp = tmp->next;
-	}
-	/* if new node is greatest and never inserted, insert now */
-	tmp->next = new;
+	aux2 = aux1->next;
+	while (aux1 && aux2 && (aux2->n < number))
+		aux1 = aux1->next, aux2 = aux1->next;
+	aux1->next = new, new->next = aux2;
 	return (new);
 }
