f8ff028a1509651d22905221931b7ed9c76acb02
kuchelimutambi <kuchelideogratius@gmail.com>
2023-05-11 06:20:05 -0700

    update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
+
+import sys
+
 if __name__ == "__main__":
-    from sys import argv
-    userin = argv[1:]
+    userin = sys.argv[1:]
     size = len(userin)
-    print("{:d} {:s}{:s}".
-          format(size,
-                 "arguments" if (size) is not 1 else "argument",
-                 "." if (size) is 0 else ":"))
+    print("{:d} {:s}{:s}".format(
+        size,
+        "arguments" if size != 1 else "argument",
+        "." if size == 0 else ":"
+    ))
     for idx, arg in enumerate(userin):
         print("{:d}: {:s}".format(idx + 1, arg))
